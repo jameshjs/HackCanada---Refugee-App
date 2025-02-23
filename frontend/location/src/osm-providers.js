907a738aa6ca1343cc2897ce80009890239ea8e9
Xiaoyang Liu <144244539+ERHUTUZI123@users.noreply.github.com>
2025-02-23 08:02:29 -0500

    Add files via upload

---
+++ frontend/location/src/osm-providers.js
@@ -0,0 +1,8 @@
+export default {
+    maptiler: {
+        url: 
+        "https://api.maptiler.com/maps/basic-v2/?key=BGW0EIBV4Hz7HowKCrdK#-0.0/18.53070/52.96341",
+        attribution: 
+        '&copy; <a href="https://www.matipler.com">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a>',
+    }
+};
