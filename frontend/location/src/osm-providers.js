30e2185a21f4dcbf0220a2fb217b6df0dd4e132f
James Hong <jameshjs@Jamess-MacBook-Pro.local>
2025-03-01 21:14:21 -0500

    push

--- frontend/location/src/osm-providers.js
+++
@@ -1,8 +0,0 @@
-export default {
-    maptiler: {
-        url: 
-        "https://api.maptiler.com/maps/basic-v2/?key=BGW0EIBV4Hz7HowKCrdK#-0.0/18.53070/52.96341",
-        attribution: 
-        '&copy; <a href="https://www.matipler.com">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a>',
-    }
-};
